a0be4f21d4a76f77aaf0f69b39fcf4623ec1ecee
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-02-26 19:36:31 +0530

    updating the header for the sign in and logout menu

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 
 const users = [
   {
-    name: "Admin User",
+    name: "Super Admin",
     email: "admin@admin.com",
     password: bcrypt.hashSync("123456", 10),
     isAdmin: true,
