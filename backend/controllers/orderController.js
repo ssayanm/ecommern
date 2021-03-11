054ec0712ba091b3b067cf5a18db478e351371be
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-03-11 09:21:52 +0530

    order screen still not working

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,9 @@
 import asyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
 
-// @desc Create new order
-// @route POST /api/orders
-//@access Private
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
@@ -18,6 +18,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
   if (orderItems && orderItems.length === 0) {
     res.status(400);
     throw new Error("No order items");
+    return;
   } else {
     const order = new Order({
       orderItems,
@@ -31,13 +32,14 @@ const addOrderItems = asyncHandler(async (req, res) => {
     });
 
     const createdOrder = await order.save();
-    res.status(201).json({ createdOrder });
+
+    res.status(201).json(createdOrder);
   }
 });
 
-// @desc Get order by ID
-// @route GET /api/orders/:id
-//@access Private
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id).populate(
     "user",
