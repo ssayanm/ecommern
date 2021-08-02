790ad6265262af900082989ff9dd8198c56cce92
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-08-02 18:09:09 +0530

    updated code

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
