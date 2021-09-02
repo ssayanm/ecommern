fa227ae1609852b3d2e153cb7d47cd3e579cbd85
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-09-02 20:07:03 +0530

    updated app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
